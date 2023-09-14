4aa3632e1a2f52ac8926fbf6f1053515e347c45f
Vladimir-gs <pedro_guzman1@ucne.edu.do>
2023-09-14 18:49:35 -0400

    Creamos la migracion

--- RegistrosTickets/DAL/Context.cs
+++ RegistrosTickets/DAL/Context.cs
@@ -6,6 +6,6 @@ namespace RegistrosTickets.DAL
     public class Context : DbContext
     {
         public DbSet<Tickets> Tickets { get; set; }
-        Context(DbContextOptions<Context> options) : base(options) { }
+        public Context(DbContextOptions<Context> options) : base(options) { }
     }
 }
