b42c09315b74ea0e9c648048f2d5049fd2cc06d1
Vladimir-gs <pedro_guzman1@ucne.edu.do>
2023-09-14 18:32:22 -0400

    Creamos nuestro contexto

---
+++ RegistrosTickets/DAL/Context.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistrosTickets.Models;
+
+namespace RegistrosTickets.DAL
+{
+    public class Context : DbContext
+    {
+        public DbSet<Tickets> Tickets { get; set; }
+        Context(DbContextOptions<Context> options) : base(options) { }
+    }
+}
