2738da8c30c73b16f2c9286daee9b9d767354f89
Vladimir-gs <pedro_guzman1@ucne.edu.do>
2023-09-15 12:00:45 -0400

    Agregamos la clase para las notificaciones

---
+++ RegistrosTickets/Extensors/Notificaciones.cs
@@ -0,0 +1,23 @@
+﻿using Radzen;
+
+namespace RegistrosTickets.Extensors
+{
+    public static class Notificaciones
+    {
+        public static void ShowNotificacion(this NotificationService notifier,
+            string titulo,
+            string mensaje,
+            NotificationSeverity severity)
+        {
+            var message = new NotificationMessage
+            {
+                Severity = severity,
+                Summary = titulo,
+                Detail = mensaje,
+                Duration = 3000
+            };
+
+            notifier.Notify(message);
+        }
+    }
+}
